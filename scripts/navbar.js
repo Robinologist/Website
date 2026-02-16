e2a998d00b884a24439625ef8002b4364a63fd44
Robinologist <112644607+Robinologist@users.noreply.github.com>
2026-02-16 16:34:48 -0500

    Implemented hamburger menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -21,3 +21,51 @@ fetch('navbar.html')
     // Log errors (e.g., if nav.html is missing)
     console.error('Error loading navigation:', error);
 });
+createMenu();
+
+// Hamburger menu button
+function createMenu()
+{
+    // Fetch the nav drawer component
+    fetch('nav-drawer.html')
+    .then(response =>
+    {
+        // Check if the request was successful
+        if (!response.ok)
+        {
+            throw new Error(`Failed to load foldout nav drawer: ${response.status}`);
+        }
+        return response.text(); // Convert response to text
+    })
+    .then(drawerHTML =>
+    {
+        // Insert the nav drawer HTML
+        const drawer = document.createElement('nav-drawer');
+        drawer.innerHTML = drawerHTML;
+        document.body.prepend(drawer);
+    })
+    .catch(error =>
+    {
+        // Log errors (e.g., if nav-drawer.html is missing)
+        console.error('Error loading nav drawer:', error);
+    });
+}
+
+drawerExtended = false;
+function toggleMenu()
+{
+    drawerExtended = !drawerExtended;
+    if (drawerExtended)
+    {
+        document.getElementById("nav-drawer").style.display = "flex";
+    }
+    else
+    {
+        document.getElementById("nav-drawer").style.display = "none";
+    }
+}
+
+window.onresize = function onResize()
+{
+    if (window.innerWidth >= 950 && drawerExtended) toggleMenu();
+}
